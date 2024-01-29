117af949e7f426a4ea38ebe01c5eb709f0ec8bda
Xi <xich0108@bu.edu>
2024-01-28 19:49:48 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -1,9 +1,16 @@
+
+
+
 class X:
     def __init__(self):
         pass
 
     def __repr__(self):
         return "X"
+
+    def evaluate(self, param):
+        return param
+        pass
 
 
 class Int:
@@ -13,14 +20,22 @@ class Int:
     def __repr__(self):
         return str(self.i)
 
+    def evaluate(self, param):
+        return self.i
+
 
 class Add:
+
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
 
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+
+    def evaluate(self, param):
+        return self.p1.evaluate(param) + self.p2.evaluate(param)
+        pass
 
 
 class Mul:
@@ -37,6 +52,10 @@ class Mul:
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
 
+    def evaluate(self, param):
+        return self.p1.evaluate(param) * self.p2.evaluate(param)
+        pass
+
 
 class sub:
     def __init__(self, p1, p2):
@@ -45,6 +64,10 @@ class sub:
 
     def __repr__(self):
         return repr(self.p1) + " - " + repr(self.p2)
+
+    def evaluate(self, param):
+        return self.p1.evaluate(param) - self.p2.evaluate(param)
+        pass
 
 
 class div:
@@ -61,12 +84,12 @@ class div:
             return repr(self.p1) + " / ( " + repr(self.p2) + " )"
         return repr(self.p1) + " / " + repr(self.p2)
 
+    def evaluate(self, param):
+        return self.p1.evaluate(param) / self.p2.evaluate(param)
+        pass
+
 
 poly = Add(Add(Int(4), Int(3)), Add(X(), Mul(Int(1), Add(Mul(X(), X()), Int(1)))))
 print(poly)
-poly = Add(Add(Int(4), Int(3)), Add(X(), Mul(Int(1), sub(Mul(X(), X()), Int(1)))))
-print(poly)
-poly = Add(Add(Int(4), Int(3)), Add(X(), div(Int(1), sub(Mul(X(), X()), Int(1)))))
-print(poly)
-poly = Add(Add(Int(4), Int(3)), Add(X(), div(Add(Int(1), Int(99)), sub(Mul(X(), X()), Int(1)))))
-print(poly)
+print(poly.evaluate(-1))
+
